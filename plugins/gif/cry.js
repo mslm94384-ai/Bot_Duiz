b54981d54722f70ef1f7c592a1445cf28d3b2010
mslm94384-ai <mslm94384@gmail.com>
2026-06-28 16:29:16 +0300

    Update cry.js

--- plugins/gif/cry.js
+++ plugins/gif/cry.js
@@ -4,11 +4,11 @@ import { gifToMp4 } from "../../system/utils.js";
 let handler = async (m, { conn }) => {
     try {
         let target = m.mentionedJid?.[0] || m.quoted?.sender;
-        if (!target) return m.reply(`*~ 💙 منشن شخص مثل /${command} @${m.sender.split('@')[0]} ❤️ ~*`);
+        if (!target) return m.reply(`⚔️ *منشن الشخص يا جندي!* 🔥\n> مثال: .ابكي @${m.sender.split('@')[0]}`);
 
         let group = await conn.groupMetadata(m.chat);
         if (!group.participants.find(p => p.id === target)) {
-            return m.reply(`*🕸️ ~ العضو مش في الجروب*`);
+            return m.reply(`💀 *العضو مش في الجروب يا ضعيف!*`);
         }
 
         const res = await Scrapy.AnimeGif("cry");
@@ -16,14 +16,23 @@ let handler = async (m, { conn }) => {
         const video = await gifToMp4(url);
         
         await conn.sendMessage(m.chat, {
-            caption: `*_@${m.sender.split('@')[0]} معاه فيديو ليك وانت تبكي @${target.split('@')[0]}_*\n> *_الانمي: ${anime_name}_*`,
+            caption: `💢 *@${m.sender.split('@')[0]} معاه فيديو ليك وانت تبكي يا @${target.split('@')[0]}* 😤\n> *_الأنمي: ${anime_name}_*\n\n> الدموع مش بتنفع في المعركة يا جندي! 🦾`,
             video: video,
             gifPlayback: true,
-            mentions: [target, m.sender]
+            mentions: [target, m.sender],
+            contextInfo: {
+                isForwarded: true,
+                forwardingScore: 1,
+                forwardedNewsletterMessageInfo: {
+                    newsletterJid: '0029VbCoE0P8aKvPbZf8hU1D@newsletter',
+                    newsletterName: '𝐄𝐑𝐈𝐍 𝐁𝐎𝐓 🐦',
+                    serverMessageId: 0
+                }
+            }
         });
 
     } catch (e) {
-        m.reply(e.message);
+        m.reply(`❌ *حدث خطأ يا جندي!* 💢\n> ${e.message}`);
     }
 }
 handler.usage = ["ابكي @منشن"];
