52fb5df7b9882ea44e40e1fb97f256a843663d1a
mslm94384-ai <mslm94384@gmail.com>
2026-06-28 17:26:23 +0300

    Update image2url.js

--- plugins/tools/image2url.js
+++ plugins/tools/image2url.js
@@ -7,27 +7,27 @@ const handler = async (m, { conn, command }) => {
   const q = m.quoted ? m.quoted : m;
   const mime = (q.msg || q).mimetype || '';
 
-  if (!mime) throw '*❲ ❤️ ❳ ~ اعمل ريبلاي علي الصوره او الفيديو أو الصوت ~ ❲ 💙 ❳ *';
+  if (!mime) throw `⚔️ *رد على الصورة أو الفيديو يا جندي!* 🔥`;
   
   const media = await q.download();
   const link = await uploadToQuax(media);
   
   await conn.sendButton(m.chat, {
     imageUrl: link,
-    bodyText: "🗃️ ~ Successful *(catbox.moe)*\n- ```" + link + "```",
-    footerText: "VENi ~ VII7",
+    bodyText: `⚔️ *تم الرفع في المعسكر!* 🦾\n- \`${link}\``,
+    footerText: "𝐄𝐑𝐈𝐍 𝐁𝐎𝐓 🐦",
     buttons: [
-      { name: "cta_copy", params: { display_text: "Copy Link", copy_code: link } },
+      { name: "cta_copy", params: { display_text: "📋 نسخ الرابط", copy_code: link } },
     ],
     mentions: [m.sender],
     newsletter: {
-      name: '𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
-      jid: '120363225356834044@newsletter'
+      name: '𝐄𝐑𝐈𝐍 𝐁𝐎𝐓 🐦',
+      jid: '0029VbCoE0P8aKvPbZf8hU1D@newsletter'
     },
     interactiveConfig: {
       buttons_limits: 10,
-      list_title: "𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️",
-      button_title: "Click Here",
+      list_title: "𝐄𝐑𝐈𝐍 𝐁𝐎𝐓 🐦",
+      button_title: "اضغط هنا",
       canonical_url: "https://vxv-profile.vercel.app"
     }
   }, m);
