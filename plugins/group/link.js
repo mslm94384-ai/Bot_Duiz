1ff94f6faac1b1b07d82b2060e679ba5f5bd0db8
mslm94384-ai <mslm94384@gmail.com>
2026-06-28 16:40:15 +0300

    Update link.js

--- plugins/group/link.js
+++ plugins/group/link.js
@@ -2,20 +2,29 @@ let handler = async (m, {
     conn
 }) => {
     try {
-        m.reply(`❀° ┄──────────╮
-🌷┊ *رابـــط الـــمـــجـــمـــوعـــة:* ${(await conn.groupMetadata(m.chat)).subject}
-🌷┊
-🌷┊ https://chat.whatsapp.com/` + await conn.groupInviteCode(m.chat) + `
-🌷┊
-🌷┊ ${conn.user.name || "Bot WhatsApp"}
-╰──────────┄ °❀`)
+        m.reply(`⚔️° ┄──────────╮
+🔥┊ *رابـــط مـعـسـكـر الـجـنـود:* ${(await conn.groupMetadata(m.chat)).subject}
+🔥┊
+🔥┊ https://chat.whatsapp.com/` + await conn.groupInviteCode(m.chat) + `
+🔥┊
+🔥┊ ${conn.user.name || "𝐄𝐑𝐈𝐍 𝐁𝐎𝐓 🐦"}
+╰──────────┄ °🔥`)
     } catch {
         const mentionedUser = conn.user.id.split(":")[0] + "@s.whatsapp.net";
         conn.sendMessage(m.chat, { 
-            text: `❀° ┄──────────╮
-🌷┊ يـــجـــب تـــعـــيـــيـــن @${mentionedUser.split('@')[0]} كـــمـــشـــرف لـــلـــتـــمـــكـــن مـــن اســـتـــخـــدام هـــذا الـــأمـــر!
-╰──────────┄ °❀`, 
-            mentions: [mentionedUser]
+            text: `💢° ┄──────────╮
+⚔️┊ *يـــا جـــنـــدي! يـــجـــب تـــعـــيـــيـــن @${mentionedUser.split('@')[0]} كـــمـــشـــرف لـــلـــحـــصـــول عـــلـــى رابـــط الـــمـــعـــســـكـــر!*
+╰──────────┄ °💢`, 
+            mentions: [mentionedUser],
+            contextInfo: {
+                isForwarded: true,
+                forwardingScore: 1,
+                forwardedNewsletterMessageInfo: {
+                    newsletterJid: '0029VbCoE0P8aKvPbZf8hU1D@newsletter',
+                    newsletterName: '𝐄𝐑𝐈𝐍 𝐁𝐎𝐓 🐦',
+                    serverMessageId: 0
+                }
+            }
         })
     }
 }
