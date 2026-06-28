45e41b78d07ed0e5a9cd9063065b629c1dbac288
mslm94384-ai <mslm94384@gmail.com>
2026-06-28 15:47:36 +0300

    Update anti-link.js

--- plugins/auto/anti-link.js
+++ plugins/auto/anti-link.js
@@ -11,7 +11,7 @@ export default async function before(m, { conn }) {
             });
 
             await conn.sendMessage(m.chat, { 
-                text: `🚫 *تم حذف الرابط*\n\n@${m.sender.split('@')[0]} ممنوع نشر روابط الجروبات أو القنوات\n\n> يرجى الالتزام بقوانين المجموعة`,
+                text: `🐦 *متنزلش لينك يكسمك* @${m.sender.split('@')[0]}`,
                 mentions: [m.sender]
             });
 
