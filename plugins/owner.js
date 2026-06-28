16fb2d270020cbb20e64457522ff673c6ad4f6b2
mslm94384-ai <mslm94384@gmail.com>
2026-06-28 17:42:02 +0300

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,31 +1,43 @@
 let handler = async (m, { conn, bot }) => {
-  let watermark = 'VA';
+  let watermark = '𝐄𝐑𝐈𝐍 𝐁𝐎𝐓 🧛';
   
   let quoted = {
     key: { fromMe: false, participant: '0@s.whatsapp.net', remoteJid: 'status@broadcast' },
-    message: { conversation: 'Pomni AI ❤️💙' }
+    message: { conversation: '𝐄𝐑𝐈𝐍 𝐁𝐎𝐓 🧛' }
   };
-  const num = bot.config.owners[0].jid.split("@")[0];
-  let vcard = `BEGIN:VCARD
+  
+  // جلب كل الأونرز
+  const owners = bot.config.owners || [];
+  const vcards = owners.map(owner => {
+    const num = owner.jid.split("@")[0];
+    return `BEGIN:VCARD
 VERSION:3.0
-FN:${watermark}
+FN:${owner.name || 'جندي مجهول'}
 TEL;type=CELL;waid=${num}:+${num}
 END:VCARD`;
+  });
 
   let img = 'https://i.pinimg.com/originals/e2/21/20/e221203f319df949ee65585a657501a2.jpg';
   
   await conn.sendMessage(m.chat, {
-    contacts: { displayName: watermark, contacts: [{ vcard }] },
+    contacts: { 
+      displayName: watermark, 
+      contacts: vcards.map(vcard => ({ vcard })) 
+    },
     contextInfo: {
       forwardingScore: 2023,
       externalAdReply: {
-        title: '𝑇𝛨𝛯 𝛩𝑊𝛮𝛯𝑅',
+        title: '⚔️ 𝐄𝐑𝐈𝐍 𝐁𝐎𝐓 🧛',
         body: watermark,
-        sourceUrl: 'https://whatsapp.com/channel/0029Vb3UUKz3QxS3bgWmTc3x',
+        sourceUrl: 'https://whatsapp.com/channel/0029VbCoE0P8aKvPbZf8hU1D',
         thumbnailUrl: img,
         mediaType: 1,
         showAdAttribution: true,
-        renderLargerThumbnail: true
+        renderLargerThumbnail: true,
+        newsletter: {
+          name: '𝐄𝐑𝐈𝐍 𝐁𝐎𝐓 🐦',
+          jid: '0029VbCoE0P8aKvPbZf8hU1D@newsletter'
+        }
       }
     }
   }, { quoted })
