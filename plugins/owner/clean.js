314b2e73eed456af3396576a67169955258dcad7
mslm94384-ai <mslm94384@gmail.com>
2026-06-28 17:06:46 +0300

    Update clean.js

--- plugins/owner/clean.js
+++ plugins/owner/clean.js
@@ -7,20 +7,20 @@ export default {
   owner: true,
   async execute(m) {
     try {
-      m.react("🧹")
-      m.reply("*🧹╎ جاري التنظيف ...*")
+      m.react("⚔️")
+      m.reply("*⚔️╎ جاري تنظيف المعسكر يا قائد...* 🔥")
       const result = execSync(
         'files=$(ls session/pre-key-* session/device-list-* 2>/dev/null | wc -l); rm -rf session/pre-key-* session/device-list-* 2>/dev/null; echo "$files"',
         { encoding: 'utf-8' }
       );
       
       const count = parseInt(result.trim()) || 0;
-      const message = `*🗑️╎  تم تنظيف [ ${count} ] ملف*`;
+      const message = `*🗑️╎  تم تنظيف [ ${count} ] ملف من المعسكر* 🦾\n\n> تاتاكاي! استمر في القتال يا قائد 🔥`;
       
       m.react("🟢")
       await m.reply(message);
     } catch (error) {
-      await m.reply(`\`\`\`${error.message}\`\`\``);
+      await m.reply(`❌ *حدث خطأ يا قائد!* 💢\n> \`\`\`${error.message}\`\`\``);
     }
   }
-}
+  }
