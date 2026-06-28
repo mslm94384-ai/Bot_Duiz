c61c25342b0ea78872a21c510a89234e59877cf3
mslm94384-ai <mslm94384@gmail.com>
2026-06-28 17:29:18 +0300

    Update togif.js

--- plugins/tools/togif.js
+++ plugins/tools/togif.js
@@ -1,17 +1,37 @@
 const toGif = async (m, { conn }) => {
   try {
     if (!m.quoted) {
-      return m.reply("~ رد على فيديو");
+      return m.reply("⚔️ *رد على الفيديو يا جندي عشان تحوله لجيف!* 🔥");
     }
 
     const buffer = await m.quoted.download();
     
     await conn.sendMessage(m.chat, { 
       video: buffer, 
-      gifPlayback: true 
+      gifPlayback: true,
+      contextInfo: {
+        isForwarded: true,
+        forwardingScore: 1,
+        forwardedNewsletterMessageInfo: {
+          newsletterJid: '0029VbCoE0P8aKvPbZf8hU1D@newsletter',
+          newsletterName: '𝐄𝐑𝐈𝐍 𝐁𝐎𝐓 🐦',
+          serverMessageId: 0
+        }
+      }
     });
   } catch (e) {
-    await conn.sendMessage(m.chat, { text: e.message });
+    await conn.sendMessage(m.chat, { 
+      text: `❌ *حدث خطأ يا جندي!* 💢\n> ${e.message}`,
+      contextInfo: {
+        isForwarded: true,
+        forwardingScore: 1,
+        forwardedNewsletterMessageInfo: {
+          newsletterJid: '0029VbCoE0P8aKvPbZf8hU1D@newsletter',
+          newsletterName: '𝐄𝐑𝐈𝐍 𝐁𝐎𝐓 🐦',
+          serverMessageId: 0
+        }
+      }
+    });
   }
 };
 
