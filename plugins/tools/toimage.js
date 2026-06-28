724531edcb5cc78522f986eac9eec0f134278f1c
mslm94384-ai <mslm94384@gmail.com>
2026-06-28 17:30:18 +0300

    Update toimage.js

--- plugins/tools/toimage.js
+++ plugins/tools/toimage.js
@@ -1,16 +1,36 @@
 const toimg = async (m, { conn }) => {
   try {
     if (!m.quoted) {
-      return m.reply("~ رد على صوره");
+      return m.reply("⚔️ *رد على الصورة يا جندي عشان تحولها!* 🔥");
     }
 
     const buffer = await m.quoted.download();
     
     await conn.sendMessage(m.chat, { 
-      image: buffer
+      image: buffer,
+      contextInfo: {
+        isForwarded: true,
+        forwardingScore: 1,
+        forwardedNewsletterMessageInfo: {
+          newsletterJid: '0029VbCoE0P8aKvPbZf8hU1D@newsletter',
+          newsletterName: '𝐄𝐑𝐈𝐍 𝐁𝐎𝐓 🐦',
+          serverMessageId: 0
+        }
+      }
     });
   } catch (e) {
-    await conn.sendMessage(m.chat, { text: e.message });
+    await conn.sendMessage(m.chat, { 
+      text: `❌ *حدث خطأ يا جندي!* 💢\n> ${e.message}`,
+      contextInfo: {
+        isForwarded: true,
+        forwardingScore: 1,
+        forwardedNewsletterMessageInfo: {
+          newsletterJid: '0029VbCoE0P8aKvPbZf8hU1D@newsletter',
+          newsletterName: '𝐄𝐑𝐈𝐍 𝐁𝐎𝐓 🐦',
+          serverMessageId: 0
+        }
+      }
+    });
   }
 };
 
